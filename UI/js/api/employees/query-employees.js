cc0a1487b27a0a1323e752e592898bc866d21f61
RD\ltdung <lehuutiendung972000@gmail.com>
2021-07-16 17:33:26 +0700

    design dropdown, handle events

--- UI/js/api/employees/query-employees.js
+++ UI/js/api/employees/query-employees.js
@@ -2,15 +2,15 @@
  * Get all employees
  */
 
-function employeeData(){
-    fetch('http://cukcuk.manhnv.net/v1/Employees').then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
-    }).catch(error => {
-        console.log(error);
-    });
+// function employeeData(){
+//     fetch('http://cukcuk.manhnv.net/v1/Employees').then(response => {
+//         return response.json();
+//     }).then(data => {
+//         console.log(data);
+//     }).catch(error => {
+//         console.log(error);
+//     });
    
-}
+// }
 
-employeeData();
+// employeeData();
--- UI/js/dropdown/dropdown.js
+++ UI/js/dropdown/dropdown.js
@@ -1,23 +1,35 @@
-// var state = document.getElementsByClassName('dropdown-data')[0].style.display;
-// document.getElementsByClassName('dropdown')[0].addEventListener('click', () => {
-//     if(state == "none"){
-//         document.getElementsByClassName('dropdown-data')[0].style.display = "block";
-//         state = "block";
-//     }else if (state == "block"){
-//         document.getElementsByClassName('dropdown-data')[0].style.display = "none";
-//         state = "none";
-//     }else{
-//         document.getElementsByClassName('dropdown-data')[0].style.display = "block";
-//         state = "block";
-//     }  
-// })
 var state = false;
-var cur = document.querySelectorAll('[data-dropdown]');
-cur.forEach(function(item){
-    item.addEventListener('click', function(){
+var listDropdown = document.querySelectorAll('[data-dropdown]');
+var x;
+listDropdown.forEach(function(dropdown){
+    dropdown.addEventListener('click', function(){
         state = !state;
+        x = dropdown.getAttribute('data-dropdown');
         if(state){
-            
+            document.querySelector(`[data-dropdown-value = "${x}"]`).style.display = "none"; 
+        }else{
+            document.querySelector(`[data-dropdown-value = "${x}"]`).style.display = "block"; 
         }
+    })
+})
+
+var listItem = document.querySelectorAll('div[data-dropdown-item]');
+listItem.forEach(function(item){
+    item.addEventListener('click', function(){
+        // document.querySelector(`div[data-dropdown-item = "${}"]`).classList.remove("active");
+        lastItemID = localStorage.getItem('lastItem');
+        y = item.getAttribute('data-dropdown-item');
+        if(lastItemID != y){
+            document.querySelector(`div[data-dropdown-item = "${lastItemID}"]`).classList.remove("active");
+            document.querySelector(`i[data-dropdown-item = "${lastItemID}"]`).style.display = "none";
+        }
+        localStorage.setItem('lastItem', y);
+        
+        // Lay gia tri text cua span khi click
+        curValue = document.querySelector(`span[data-dropdown-item = "${y}"]`).textContent;
+        document.querySelector(`p[data-dropdown-item = "${x}"]`).innerHTML = curValue;
+        document.querySelector(`div[data-dropdown-item = "${y}"]`).classList.add("active");
+        document.querySelector(`i[data-dropdown-item = "${y}"]`).style.display = "block";
+       
     })
 })
