8ec3ce0a5783e1cb80b7de73dda1af4abe631259
RD\ltdung <lehuutiendung972000@gmail.com>
2021-07-16 13:34:18 +0700

    Add component dropdown

---
+++ UI/js/api/employees/query-employees.js
@@ -0,0 +1,16 @@
+/**
+ * Get all employees
+ */
+
+function employeeData(){
+    fetch('http://cukcuk.manhnv.net/v1/Employees').then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.log(error);
+    });
+   
+}
+
+employeeData();
--- UI/js/dropdown/dropdown.js
+++ UI/js/dropdown/dropdown.js
@@ -1,13 +1,23 @@
-var state = document.getElementsByClassName('dropdown-data')[0].style.display;
-document.getElementsByClassName('dropdown')[0].addEventListener('click', () => {
-    if(state == "none"){
-        document.getElementsByClassName('dropdown-data')[0].style.display = "block";
-        state = "block";
-    }else if (state == "block"){
-        document.getElementsByClassName('dropdown-data')[0].style.display = "none";
-        state = "none";
-    }else{
-        document.getElementsByClassName('dropdown-data')[0].style.display = "block";
-        state = "block";
-    }  
+// var state = document.getElementsByClassName('dropdown-data')[0].style.display;
+// document.getElementsByClassName('dropdown')[0].addEventListener('click', () => {
+//     if(state == "none"){
+//         document.getElementsByClassName('dropdown-data')[0].style.display = "block";
+//         state = "block";
+//     }else if (state == "block"){
+//         document.getElementsByClassName('dropdown-data')[0].style.display = "none";
+//         state = "none";
+//     }else{
+//         document.getElementsByClassName('dropdown-data')[0].style.display = "block";
+//         state = "block";
+//     }  
+// })
+var state = false;
+var cur = document.querySelectorAll('[data-dropdown]');
+cur.forEach(function(item){
+    item.addEventListener('click', function(){
+        state = !state;
+        if(state){
+            
+        }
+    })
 })
